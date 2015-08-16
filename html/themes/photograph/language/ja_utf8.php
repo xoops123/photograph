79ea791508e8b0c5a3775f0abcfbac2ef3a4b1db
xoops123 <marine@mistgreen.net>
2015-08-16 17:28:13 +0900

    New up (Ver1.01)

---
+++ html/themes/photograph/language/ja_utf8.php
@@ -0,0 +1,38 @@
+<?php
+//global
+define('_gallery_EVENT_INFO', 'イベント通知設定');
+define('_gallery_WELCOME', 'ようこそ');
+define('_gallery_WELCOME_2', 'さん！');
+define('_gallery_HOME_NAME', 'ホーム');
+define('_gallery_LOGIN', 'ログイン：');
+define('_gallery_LOGIN_BUTTON', ' OK ');
+define('_gallery_LOGIN_FORM', ' ログイン ');
+define('_gallery_PWORD', 'パスワード：');
+define('_gallery_ADMIN_PAGE', '管理ページ');
+define('_gallery_LOGOUT', 'ログアウト');
+define('_gallery_LOST_PASS', 'パスワード紛失');
+define('_gallery_RESISTER', '新規登録しませんか？');
+define('_gallery_REM', '記憶する');
+define('_gallery_NORTIFICATION', 'イベント情報');
+define('_gallery_USER_ACCOUNT', 'アカウント情報');
+define('_gallery_EDIT_ACCOUNT', '編集');
+define('_gallery_VIEWPMSG', '受信箱');
+define('_gallery_USER_NAME', 'ユーザー名');
+define('_gallery_NOMAL', '標準');
+define('_gallery_BIG', '拡大');
+define('_gallery_BASIC', '基本スタイル');
+define('_gallery_JAN', '１月');
+define('_gallery_FEB', '２月');
+define('_gallery_MAR', '３月');
+define('_gallery_APR', '４月');
+define('_gallery_MAY', '５月');
+define('_gallery_JUN', '６月');
+define('_gallery_JUL', '７月');
+define('_gallery_AUG', '８月');
+define('_gallery_SEP', '９月');
+define('_gallery_OCT', '１０月');
+define('_gallery_NOV', '１１月');
+define('_gallery_DEC', '１２月');
+define('_gallery_SEARCH', 'サイト内検索');
+
+?>
---
+++ html/themes/photograph/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="photograph"
+Depends=Legacy_RenderSystem,legacy
+Url="http://xoops123.com/"
+Version="1.01"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=marine/mistgreen
+ScreenShot="screenshot.png"
+Description="width 100% Valid XHTML 1.0 Transitional"
+W3C=YES
+
+Licence="creative commons"
+
+*/
+?>
